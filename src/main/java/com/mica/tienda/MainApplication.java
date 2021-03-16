9b5d8c6692aa77aa08ca1d4cb9b5ba46083e3550
MicaFurno <miqui.furno@gmail.com>
2021-03-16 15:34:27 -0300

    Actualizada clase Producto

--- src/main/java/com/mica/tienda/MainApplication.java
+++ src/main/java/com/mica/tienda/MainApplication.java
@@ -1,6 +1,7 @@
 package com.mica.tienda;
 
 import com.mica.tienda.entidades.Persona;
+import com.mica.tienda.entidades.Producto;
 
 public class MainApplication {
 
@@ -12,10 +13,21 @@ public class MainApplication {
 				.setEmail("miqui.furno@gmail.com")
 				.setDni(39393999)
 				.build();
-			
+
+		Producto prod = Producto.builder()
+								.setCodigo("S16")
+								.setNombreProducto("Krishna Leela")
+								.setPrecioUnitario(460)
+								.setStock(5)
+								.build();
+		
 System.out.println(per.toString());
+System.out.println(prod.toString());
 
 	}
 	
+	
+	
+ 
 
 }
--- src/main/java/com/mica/tienda/entidades/Producto.java
+++ src/main/java/com/mica/tienda/entidades/Producto.java
@@ -5,6 +5,17 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(setterPrefix = "set")
+@ToString
+@Data
 @Entity
 public class Producto implements Serializable{
 	private static final Long SerialVersionUID = 1L;
@@ -19,5 +30,14 @@ public class Producto implements Serializable{
 	private String color;
 	private int precioUnitario;
 	private int stock;
+	
+	public String getStockDisponible() {
+		if (stock > 0) {
+			return("Stock disponible");
+		}else{
+			return("AGOTADO");
+		}
+	
+	}
 }
 
