dc7c38c170c64d41c0c9627d547e669613bae25b
MicaFurno <miqui.furno@gmail.com>
2021-03-16 15:01:27 -0300

    Agregada la clase Producto

--- src/main/java/com/mica/tienda/entidades/Administrador.java
+++ src/main/java/com/mica/tienda/entidades/Administrador.java
@@ -1,10 +1,12 @@
 package com.mica.tienda.entidades;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class Administrador {
+public class Administrador implements Serializable {
 	
 	@Id
 	private Long id;
---
+++ src/main/java/com/mica/tienda/entidades/Producto.java
@@ -0,0 +1,22 @@
+package com.mica.tienda.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Producto implements Serializable{
+	private static final Long SerialVersionUID = 1L;
+
+	
+	@Id
+	private Long id;
+	
+	private String codigo;
+	private String nombreProducto;
+	private String descripcion;
+	private String color;
+	private int precioUnitario;
+	private int stock;
+}
